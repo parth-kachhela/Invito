1b84a83920fe397a83258da4e11a41b96a99e694
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-26 18:27:34 +0530

    init

---
+++ Backend/src/db/Schema.ts
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+
+interface EventInterface {
+  eventId: string;
+  name: string;
+  description: string;
+  vanue: string;
+  date: Date;
+  time: string;
+  guest: [];
+  createdAt: Date;
+  expiresAt: Date;
+}
+
+const EventSchema = new Schema(
+  {
+    eventId: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    vanue: {
+      type: String,
+      require: true,
+    },
+    date: {
+      type: String,
+      require: true,
+    },
+    time: {
+      type: String,
+      require: true,
+    },
+    guests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Guest",
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const EventModel = mongoose.model("Event", EventSchema);
