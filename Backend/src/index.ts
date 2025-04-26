10c3c01900fb3f0e6ce4af104290f38f2986d57e
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-26 18:33:05 +0530

    init

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,17 @@
 import express from "express";
 import { CreateEvent } from "./Routes/CreateEvent";
+import mongoose from "mongoose";
+import { MONGODB_URL } from "./config";
 const app = express();
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("db connected..!");
+  })
+  .catch((e) => {
+    console.log("db Error : " + e);
+  });
 
 app.get("/", (req, res) => {
   res.json({
