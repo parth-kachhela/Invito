7631c6f75975c143436498e0fde0e457642319ca
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-27 11:20:10 +0530

    init

---
+++ Frontend/src/components/ui/AddGuest.tsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import axios from "axios";
+import { BACKEND_URL } from "../../../config";
+import { useNavigate } from "react-router-dom";
+
+export function CreateContent() {
+  const nameRef = useRef<HTMLInputElement>(undefined);
+  const emailRef = useRef<HTMLInputElement>(undefined);
+  const navigate = useNavigate();
+  async function CreateApi() {
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const eventId = localStorage.getItem("eventId");
+    const ans = await axios.post(`${BACKEND_URL}/api/v1/add`, {
+      name: name,
+      email: email,
+      eventId: eventId,
+    });
+    //@ts-ignore
+    let id = ans.data?.m;
+    navigate("/dashboard");
+  }
+  return (
+    <div className="h-screen w-screen flex justify-center items-center bg-gray-200">
+      <div className="flex flex-col justify-center  items-center bg-white rounded-md max-h-full max-w-full gap-3 p-4 shadow-sm">
+        <h1 className="text-xl">Event Detials :</h1>
+        <Input placeholder="Name of event" ref={nameRef} />
+        <Input placeholder="email of event" ref={emailRef} />
+        <Button
+          variant="secondray"
+          size="lg"
+          text="Submit"
+          onClick={CreateApi}
+        ></Button>
+      </div>
+    </div>
+  );
+}
