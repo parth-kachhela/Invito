ed43089354f6d7d8b571c2d0ed2475c36a83e8fa
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-29 09:47:32 +0530

    init

---
+++ Backend/src/Routes/getEventDetails.ts
@@ -0,0 +1,23 @@
+// Get Event Details API
+
+import { EventModel } from "../db/Schema";
+
+export const getEventDetails = async (req: any, res: any) => {
+  try {
+    const { eventId } = req.query; // query se lena
+    if (!eventId) {
+      return res.status(400).json({ error: "Event ID is required" });
+    }
+
+    const event = await EventModel.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
+    }
+
+    return res.status(200).json({ event });
+  } catch (error) {
+    console.error("Error fetching event details", error);
+    return res.status(500).json({ error: "Failed to fetch event details" });
+  }
+};
